590c97a5c8dd7e39becb7baa74b327e23f88103a
Bruno De Masi <brunodemasi1@gmail.com>
2023-03-29 00:21:31 -0300

    feat: tailwind + coming soon page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    colors: {
+      primary: '#57C5B6',
+      secondary: '#1A5F7A',
+      dark: '#002B5B',
+      light: '#fafafa',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
