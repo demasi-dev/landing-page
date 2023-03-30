01c96a34e943a8b7d7b1a622161847ef575eb165
Bruno De Masi <brunodemasi1@gmail.com>
2023-03-30 16:07:27 -0300

    feat: aws amplify prep

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css';
+import { Amplify } from 'aws-amplify';
 import type { AppProps } from 'next/app';
 import { Raleway } from 'next/font/google';
+
+import '@/styles/globals.css';
+import awsExports from '../aws-exports';
+
+Amplify.configure({ ...awsExports, ssr: true });
 
 const raleway = Raleway({ subsets: ['latin'] });
 
